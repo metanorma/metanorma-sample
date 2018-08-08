09ac4ca8ebf6284a40d577f50293e8df33f6c5bb
Nick Nicholas <opoudjis@optusnet.com.au>
2018-08-08 22:34:30 +1000

    ruby 2.3

--- spec/isodoc/rsdconvert_spec.rb
+++ spec/isodoc/rsdconvert_spec.rb
@@ -240,9 +240,6 @@ RSpec.describe IsoDoc::Sample do
         <introduction id="B" obligation="informative"><title>Introduction</title><clause id="C" inline-header="false" obligation="informative">
          <title>Introduction Subsection</title>
        </clause>
-       <patent-notice>
-       <p>This is patent boilerplate</p>
-       </patent-notice>
        </introduction></preface><sections>
        <clause id="D" obligation="normative">
          <title>Scope</title>
@@ -283,9 +280,6 @@ RSpec.describe IsoDoc::Sample do
          <title>Annex A.1a</title>
          </clause>
        </clause>
-              <appendix id="Q2" inline-header="false" obligation="normative">
-         <title>An Appendix</title>
-       </appendix>
        </annex><bibliography><references id="R" obligation="informative">
          <title>Normative References</title>
        </references><clause id="S" obligation="informative">
@@ -305,11 +299,10 @@ RSpec.describe IsoDoc::Sample do
              </div>
              <br/>
              <div class="Section3" id="B">
-               <h1 class="IntroTitle">0.&#160; Introduction</h1>
+               <h1 class="IntroTitle">Introduction</h1>
                <div id="C">
-          <h2>0.1. Introduction Subsection</h2>
+          <h2>Introduction Subsection</h2>
         </div>
-               <p>This is patent boilerplate</p>
              </div>
              <p class="zzSTDTitle1"/>
              <div id="D">
@@ -356,9 +349,6 @@ RSpec.describe IsoDoc::Sample do
           <div id="Q1">
           <h3>A.1.1. Annex A.1a</h3>
           </div>
-        </div>
-               <div id="Q2">
-          <h2>Appendix 1. An Appendix</h2>
         </div>
              </div>
              <br/>

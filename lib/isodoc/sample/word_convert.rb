a5f166dfdd9040c0c5b441717690b71ef6851c3a
Nick Nicholas <opoudjis@optusnet.com.au>
2018-11-17 21:24:36 +1100

    allow document prefaces in Word

--- lib/isodoc/sample/word_convert.rb
+++ lib/isodoc/sample/word_convert.rb
@@ -47,15 +47,6 @@ module IsoDoc
         end
       end
 
-      def make_body2(body, docxml)
-        body.div **{ class: "WordSection2" } do |div2|
-          info docxml, div2
-          div2.p { |p| p << "&nbsp;" } # placeholder
-        end
-        #body.br **{ clear: "all", style: "page-break-before:auto;mso-break-type:section-break;" }
-        section_break(body)
-      end
-
       def info(isoxml, out)
         @meta.security isoxml, out
         super
